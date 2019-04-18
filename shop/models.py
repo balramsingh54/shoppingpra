738e621294e5d84818abf61e246e5d491302bb86
Balram <singhbalram054@gmail.com>
2019-04-18 20:11:56 +0530

    running of program

--- shop/models.py
+++ shop/models.py
@@ -39,3 +39,7 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('shop:product_detail', args=[self.id, self.slug])
+
+
+
+        
--- shop/views.py
+++ shop/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
-from cart.forms import CartAddProductForm
 
 # Create your views here.
 
@@ -14,7 +13,8 @@ def product_list(request, category_slug=None):
     if category_slug:
         category=get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request,'', {'category':category,
+    return render(request,'shop/products/list.html', {'category':category,
     							'categories':categories,
     							'products':products})
+
 
