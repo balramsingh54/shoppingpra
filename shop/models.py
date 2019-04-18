a255d9c458f02f99d5af361fccd9a33c01a22b7e
Balram <singhbalram054@gmail.com>
2019-04-18 23:27:14 +0530

    poduct added and run

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.urls import reverse
 # Create your models here.
+
+
+
 class Category(models.Model):
     name= models.CharField(max_length=200, db_index=True)
     slug= models.SlugField(max_length=200, db_index=True, unique=True)
@@ -37,9 +40,8 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
-        return reverse('shop:product_detail', args=[self.id, self.slug])
+    #def get_absolute_url(self):
+     #   return reverse('shop:product_detail', args=[self.id, self.slug])
 
 
 
-        
--- shop/urls.py
+++ shop/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('', views.product_list, name= 'product_list'),
 
     path('<str:category_slug>', views.product_list, name='product_list_by_category'),
-
     ]
